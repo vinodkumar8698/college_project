1c71f3fb9b6ea0851098b4dc79a9d4ed16bca628
vinodkumar kanike <83947511+vinodkumar8698@users.noreply.github.com>
2022-12-26 10:06:30 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const PORT = 5000
+
+app.use(express.json())
+app.get("/", (req, res) => {
+    res.status(200).send('hello from backend server')
+})
+app.get("/about", (req, res) => {
+    res.status(200).send('hello from backend server')
+})
+
+app.listen(PORT, () => {
+    console.log(`server is started on port ${PORT}` || 8080)
+})
